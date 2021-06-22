3f62e52ee5b9ed81c296eafb2fa722bc41096907
benlf1 <54038475+benlf1@users.noreply.github.com>
2021-06-22 13:03:49 -0700

    Make chair game

--- Asset-Shuffle-5/Assets/Scripts/CameraController.cs
+++ Asset-Shuffle-5/Assets/Scripts/CameraController.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-Original script from krenuds on Reddit
-https://www.reddit.com/r/Unity3D/comments/8k7w7v/unity_simple_mouselook/
-
-Modified to only rotate camera with right click
+Tutorial from https://youtu.be/_QajrabyTJc
 */
 public class CameraController : MonoBehaviour 
 {
-	Vector2 rotation = Vector2.zero;
-	public float speed = 3;
+    public float mouseSensitivity = 300f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
 	void Update () {
-        if (Input.GetButton("Fire2")) // using Input Manager for right click
-        {
-            rotation.y += Input.GetAxis("Mouse X");
-            rotation.x += -Input.GetAxis("Mouse Y");
-            transform.eulerAngles = (Vector2)rotation * speed;
-        }
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
 	}
 
 }
